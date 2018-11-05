dd0c1bd962481b310c8389c2837df45037cf0729
Lukasz Majewski <lukma@denx.de>
2018-11-05 17:06:19 +0100

    y2038: Provide struct_itimierspec64.h
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

--- include/bits/types/struct_itimerspec64.h
+++ include/bits/types/struct_itimerspec64.h
@@ -1,1 +1,15 @@
-#include <time/bits/types/struct_itimerspec64.h>
+#ifndef __itimerspec64_defined
+#define __itimerspec64_defined 1
+
+#include <bits/types.h>
+#include <bits/types/struct_timespec.h>
+#include <bits/types/struct_timespec64.h>
+
+/* POSIX.1b structure for timer start values and intervals.  */
+struct __itimerspec64
+{
+  struct __timespec64 it_interval;
+  struct __timespec64 it_value;
+};
+
+#endif

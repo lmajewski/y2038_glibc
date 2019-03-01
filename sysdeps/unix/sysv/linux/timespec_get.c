91697830200604f249200f51e1fb5733e0adb536
Lukasz Majewski <lukma@denx.de>
2019-03-01 08:11:01 +0100

    y2038: timespec_get: Provide Y2038 safe implementation
    This commit brings the timespec_get64 version of this syscall. When
    one fails call it - the non-Y2038 safe version is called.
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

--- sysdeps/unix/sysv/linux/timespec_get.c
+++ sysdeps/unix/sysv/linux/timespec_get.c
@@ -46,22 +46,30 @@ timespec_get (struct timespec *ts, int base)
 }
 
 /* 64-bit time version */
-
-/* We don't have a 64-bit-time syscall yet, so just convert arguments
- * between 64-bit and 32-bit time, and use the 32-bit implementation.
- * 
- * We could do the reverse and make the 32-bit time implementation a
- * wrapper around the 64-bit-time implementation, but then 32-bit-time
- * uses would incur two conversions instead of zero right now.
- */
 int
-__timespec_get64 (struct __timespec64 *ts, int base)
+__timespec_get64 (struct __timespec64 *ts64, int base)
 {
-  struct timespec ts32;
-  int res = timespec_get (&ts32, base);
-  if (res != base)
+  switch (base)
     {
-      timespec_to_timespec64(&ts32, ts);
+    int result;
+    case TIME_UTC:
+	result = INLINE_SYSCALL (clock_gettime64, 2, CLOCK_REALTIME, ts64);
+	if (result == 0)
+		return base;
+
+	if (result == -1 && errno == ENOSYS) {
+		struct timespec ts32;
+		result = timespec_get (&ts32, base);
+		if (result == 0) {
+			timespec_to_timespec64(&ts32, ts64);
+			return base;
+		}
+	}
+	break;
+
+    default:
+      return 0;
     }
-  return res;
+
+  return 0;
 }

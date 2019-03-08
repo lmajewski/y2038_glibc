a7bf0e47d3ef7bae832cc4ea2cb27b0b119b25ac
Lukasz Majewski <lukma@denx.de>
2019-03-08 16:19:09 +0100

    y2038: stat: Provide the same stat struct layout as in include/bits/stat.h
    Without this change the passed (and coerced) structure gets corrupted due
    to added padding.
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -8,13 +8,14 @@
 struct __stat64_t64
   {
     __dev_t st_dev;			/* Device.  */
-
+    unsigned short int __pad1;
     __ino_t __st_ino;			/* 32bit file serial number.	*/
     __mode_t st_mode;			/* File mode.  */
     __nlink_t st_nlink;			/* Link count.  */
     __uid_t st_uid;			/* User ID of the file's owner.	*/
     __gid_t st_gid;			/* Group ID of the file's group.*/
     __dev_t st_rdev;			/* Device number, if device.  */
+    unsigned short int __pad2;
     __off64_t st_size;			/* Size of file, in bytes.  */
     __blksize_t st_blksize;		/* Optimal block size for I/O.  */
 

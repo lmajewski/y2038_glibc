HemenwayRonald/SEGV-hunt

login: Add 64-bit time support to utmp/utmpx
The new struct has the same size for 32-bit and 64-bit architecture with
two main differences compared to the 32-bit time one:

  1. ut_session is now 64-bit and it is mainly to set the ut_tv member
     alignment to be similar on both 32-bit and 64-bit architecture
     (for architectures that support multiple ABIs with different
     wordsize, such as x86 and powerpc).

  2. The internal tv_sec and tv_usec for ut_tv are now 64-bit.  Although
     it does not fully fix BZ#17470 nor BZ#18235, it might allow define
     the type as 'struct timeval' for __TIMESIZE=64 (thus fixing the
     aforementioned bug in this build configuration).

Different than laid out on the Y2038 Proofness Design [1], the
'utmp.trans' strategy is not used.  Instead:

  - New file names are defined for _PATH_UTMP, _PATH_WTMP, and
    _PATH_UTMP_DB (if applicable) and used as default for the new 2.34
    utmp{x} symbols.

  - The new utmp{x} symbols read/write 64-bit time_t entries as default.
    However if the old _PATH_UTMP or _PATH_WTMP is passed on
    utm{x}pname or updwtmp, the implementation read 32-bit utmp{x}
    entries and convert it to 64-bit ones.

  - The compat symbols read/write 32-bit time_t entries as default.
    If the default _PATH_UTMP or _PATH_WTMP is passed on
    utm{x}pname or updwtmp, the implementation reads 64-bit entries
    and convert to 32-bit ones.

The idea is not to maintain multiple databases with different formats
(which has underlying issues due to complexity, how to handle entries
that might overflow, and increases the security surface of BZ#24492),
but rather to move new application to use y2038 entries (which currently
affects 64-bit architecture as well, modulo s390).

If required the system might provide a tool to convert the old format
to newer one by opening the old file with utmpname and copying the
entries to the new format with updwtmp.

Also, for new 64-bit databases the path is not redirected to u

login/login.c
@@ -23,6 +23,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <utmp.h>
+#include <utmp-compat.h>
+#include <shlib-compat.h>
 
 
 /* Return the result of ttyname in the buffer pointed to by TTY, which should
@@ -78,7 +80,7 @@ tty_name (int fd, char **tty, size_t buf_len)
 }
 
 void
-login (const struct utmp *ut)
+__login (const struct utmp *ut)
 {
 #ifdef PATH_MAX
   char _tty[PATH_MAX + UT_LINESIZE];
@@ -137,3 +139,9 @@ login (const struct utmp *ut)
   /* Update the WTMP file.  Here we have to add a new entry.  */
   updwtmp (_PATH_WTMP, &copy);
 }
+hidden_def (__login)
+#if SHLIB_COMPAT(libutil, GLIBC_2_0, UTMP_COMPAT_BASE)
+versioned_symbol (libutil, __login, login, UTMP_COMPAT_BASE);
+#else
+weak_alias (__login, login)
+#endif

login/login32.c
@@ -1,5 +1,5 @@
-/* Copyright (C) 2008-2021 Free Software Foundation, Inc.
-   Contributed by Andreas Krebbel <Andreas.Krebbel@de.ibm.com>.
+/* Write utmp and wtmp entries, 32-bit time compat version.
+   Copyright (C) 2008-2021 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -17,14 +17,22 @@
    <https://www.gnu.org/licenses/>.  */
 
 #include <utmp.h>
-#include <utmpx.h>
+#include <utmp-compat.h>
+#include <shlib-compat.h>
 
-#include "utmp-compat.h"
+#include "utmp32.h"
+#include "utmp-convert.h"
 
-#undef weak_alias
-#define weak_alias(n,a)
-#define pututxline __pututxline
-#include "login/pututxline.c"
-#undef pututxline
+#if SHLIB_COMPAT(libutil, GLIBC_2_0, UTMP_COMPAT_BASE)
+/* Write the given entry into utmp and wtmp.  */
+void
+login32 (const struct utmp32 *entry)
+{
+  struct utmp in64;
 
-default_symbol_version (__pututxline, pututxline, UTMP_COMPAT_BASE);
+  __utmp_convert32to64 (entry, &in64);
+  __login (&in64);
+}
+
+compat_symbol (libutil, login32, login, GLIBC_2_0);
+#endif

lmajewski/y2038_glibc

login/login.c
@@ -24,6 +24,7 @@
 #include <stdlib.h>
 #include <utmp.h>
 #include <shlib-compat.h>
+#include <utmp-compat.h>
 
 /* Return the result of ttyname in the buffer pointed to by TTY, which should
    be of length BUF_LEN.  If it is too long to fit in this buffer, a
@@ -137,9 +138,9 @@ __login (const struct utmp *ut)
   /* Update the WTMP file.  Here we have to add a new entry.  */
   __updwtmp (_PATH_WTMP, &copy);
 }
-versioned_symbol (libc, __login, login, GLIBC_2_34);
-libc_hidden_ver (__login, login)
+libc_hidden_def (__login)
+versioned_symbol (libc, __login, login, UTMP_COMPAT_BASE);
 
-#if OTHER_SHLIB_COMPAT (libutil, GLIBC_2_0, GLIBC_2_34)
+#if OTHER_SHLIB_COMPAT (libutil, GLIBC_2_0, UTMP_COMPAT_BASE)
 compat_symbol (libutil, __login, login, GLIBC_2_0);
 #endif

login/login32.c
@@ -1,5 +1,5 @@
-/* Copyright (C) 2008-2021 Free Software Foundation, Inc.
-   Contributed by Andreas Krebbel <Andreas.Krebbel@de.ibm.com>.
+/* Write utmp and wtmp entries, 32-bit time compat version.
+   Copyright (C) 2008-2021 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -16,17 +16,26 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#include <stdlib.h>
-#include <utmp.h>
+#ifdef SHARED
+# include <sys/types.h>
+# include <utmp.h>
+# include <libc-symbols.h>
+# include <utmp-compat.h>
+# include <shlib-compat.h>
 
-#include "utmp-compat.h"
+# include "utmp32.h"
+# include "utmp-convert.h"
 
-#if defined SHARED
-# undef weak_alias
-# define weak_alias(n,a)
-#endif
-#include "login/getutline.c"
+#if SHLIB_COMPAT(libutil, GLIBC_2_0, UTMP_COMPAT_BASE)
+/* Write the given entry into utmp and wtmp.  */
+void
+login32 (const struct utmp32 *entry)
+{
+  struct utmp in64;
 
-#if defined SHARED
-default_symbol_version (__getutline, getutline, UTMP_COMPAT_BASE);
+  __utmp_convert32to64 (entry, &in64);
+  __login (&in64);
+}
+
+compat_symbol (libc, login32, login, GLIBC_2_0);
 #endif